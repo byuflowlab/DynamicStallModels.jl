bfa511d17fefb0036ebdb0bb886b2edf07e4e2fd
JacobChild <98500878+JacobChild@users.noreply.github.com>
2023-04-07 10:36:30 -0600

    OyeLarsenTests changes

--- test/OyeLarsenTests.jl
+++ test/OyeLarsenTests.jl
@@ -13,43 +13,41 @@ using Test, DynamicStallModels, DelimitedFiles, OpenFASTsr, Plots, CCBlade, FLOW
 dsm = DynamicStallModels
 of = OpenFASTsr
 
-
-#File to test if it is still good 
-include("../testing/Oye/OyeComparer.jl")
-
 #Come back to here 
 path = dirname(@__FILE__)
 cd(path)
 #Setup to test the Oye/Larsen separation point functions (attachment degree f, and critical alphas (alphasep, alpha0?))
 
 #Extract polar
-Naca43618PolarTest = readdlm("../polars/LarsenNACA43618ClPolar.csv", ',') #read in the polar from Larsen
+Naca43618PolarTest = readdlm("../polars/LarsenFig4bClStaticPlot.csv", ',') #read in the polar from Larsen
 Naca43618PolarTest[:,1] = Naca43618PolarTest[:,1] .* pi/180 #convert to radians
 Naca43618PolarTest = [Naca43618PolarTest zeros(size(Naca43618PolarTest[:,1]))]
 #Extend polar
+#TODO before extending add a point at [0,0]
 cr75 = .2 #chord/blade radius ratio at 75%
 aoa, Cl, Cd = viterna(Naca43618PolarTest[:,1], Naca43618PolarTest[:,2], Naca43618PolarTest[:,3], cr75)
-Cl[1] = Cl[2] - (Cl[3] - Cl[2]) #! fix this later? a cop out fix as the first value is really bad
+#Cl[1] = 0.0 #Cl[2] - (Cl[3] - Cl[2]) #! fix this later? a cop out fix as the first value is really bad
 polar = [aoa Cl Cd zeros(size(aoa))]
 #Extract Fdyn data
 FdynTest = readdlm("../testing/Oye/Outputs/LarsenFig4cFdynAttachment.csv", ',') #read in the Fdyn data from Larsen
 #Quick for loop to make all Fdyn values close to 1 = 1 and values close to 0 = 0
-for i in 1:length(FdynTest[:,2])
+#=for i in 1:length(FdynTest[:,2])
     if FdynTest[i,2] < .01
         FdynTest[i,2] = 0.0
     elseif FdynTest[i,2] > .99
         FdynTest[i,2] = 1.0
     end
-    println(FdynTest[i,2])
+    #println(FdynTest[i,2])
 end
+=#
 FdynTestAk = Akima(FdynTest[:,1], FdynTest[:,2]) #make the akima interpolant
 #Make airfoil struct
 afTest = dsm.airfoil(polar; A = .07, sfun=dsm.LSP()) #make the airfoil struct
-afTest = dsm.update_airfoil(afTest; alphasep=[afTest.alphasep[1], 32.0*pi/180] ) #update the airfoil with Larsen's separation point
+afTest = dsm.update_airfoil(afTest; alphasep=[afTest.alphasep[1], 32.0*pi/180], dcldalpha = 2* pi, dcndalpha = 2 * pi ) #update the airfoil with Larsen's separation point
 airfoilsTest = Array{Airfoil, 1}(undef, 1) #make an array of the type Airfoil struct
 airfoilsTest[1] = afTest #put the airfoil into the array
 #Make the Oye struct and setup to solve
-dsmodelTest = Oye(Indicial(), 1, airfoils,1,4) #makes the struct, says it will solve it indicially and that there is 1 airfoil
+dsmodelTest = Oye(Indicial(), 1, airfoilsTest,1,4) #makes the struct, says it will solve it indicially and that there is 1 airfoil
 
 #Test the Oye/Larsen separation point functions (attachment degree f, and critical alphas (alphasep, alpha0?))
 
@@ -75,7 +73,7 @@ TestTolerance = .01 #tolerance for the tests
         print("f af at 2 deg: ", dsm.separationpoint.(Ref(afTest), alphaTestdeg[2] .* pi/180), " fdynLarsen at 2 deg: ", FdynTestAk(alphaTestdeg[2] .* pi/180))
     end
     #Test set to test the critical alphas (alphasep, alpha0?)
-    @testset "critical alphas" begin
+    @testset "critical alphas" begin #! where did these values come from?
         @test isapprox(afTest.alphasep, [-0.793985783199363, 0.5585053606381855 ], rtol = TestTolerance) #? should I do something about the low separation point at -45deg?
         @test isapprox(afTest.alpha0, -0.01982011981227349, rtol = TestTolerance) #! failing
         @test isapprox(afTest.dcldalpha, 6.634842198285429, rtol = TestTolerance) #! failing
